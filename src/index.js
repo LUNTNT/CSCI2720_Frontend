145f5ab62f75d72f10fa36f6829e0b5d8c665e0a
Lok Cheuk <chunloklok912@gmail.com>
2022-12-07 17:32:31 +0800

    login page

--- src/index.js
+++ src/index.js
@@ -7,41 +7,20 @@ import {
   Link,
 } from 'react-router-dom';
 
+import Login from './screens/Login/login'
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/about">About</Link> </li>
-          </ul>
-        </div>
+export default function App () {
+  return (
+    <BrowserRouter>
 
-        <hr />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+      <Routes>
+        {/* add path here */}
+        {/* first page is login, so add "index" */}
+        <Route index element={<Login/>}/> 
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-class Home extends React.Component {
-  render() {
-    return <h2>Home</h2>;
-  }
-}
-
-class About extends React.Component {
-  render() {
-    return <h2>About</h2>;
-  }
-}
-
-
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 root.render(<App />);
