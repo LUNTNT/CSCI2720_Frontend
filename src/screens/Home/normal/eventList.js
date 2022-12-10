0fc617bbf98aa3a87a012be3139b77d994ff36db
ysk322 <yangsiqi0322@gmail.com>
2022-12-10 22:31:30 +0800

    event list

---
+++ src/screens/Home/normal/eventList.js
@@ -0,0 +1,23 @@
+import {locationList, Locationlist} from "./locationList"
+
+export const eventList = [
+    {   
+        id: locationList[0].id,
+        event:["Shaw Sing","Shawlane",]
+    },
+    {   
+        id: locationList[1].id,
+        event:["Shaw Sing","Shawlane",]
+    },
+    {   
+        id: locationList[2].id,
+        event:["Shaw Sing","Shawlane",]
+    },
+    {   
+        id: locationList[3].id,
+        event:["Shaw Sing","Shawlane",]
+    },
+
+
+        
+]
