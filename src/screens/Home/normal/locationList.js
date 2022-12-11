9f21cfddbd41bee96037acc1f74f70e602e30e7f
ysk322 <yangsiqi0322@gmail.com>
2022-12-11 11:51:17 +0800

    update for favourite location

--- src/screens/Home/normal/locationList.js
+++ src/screens/Home/normal/locationList.js
@@ -2,22 +2,26 @@ export const locationList = [
     {
         id: 1,
         location: "shaw",
-        noofevent: 1
+        noofevent: 1,
+        fav: 1,
     },
     {
         id: 2,
         location: "na",
-        noofevent: 4
+        noofevent: 4,
+        fav: 0,
     },
     {
         id: 3,
         location: "uc",
-        noofevent: 11
+        noofevent: 11,
+        fav: 1,
     },
     {
         id: 4,
         location: "mc",
-        noofevent: 2
+        noofevent: 2,
+        fav: 0,
     },
 
 ]
