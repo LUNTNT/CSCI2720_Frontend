73036cbfa3b420ec47e583f4281f4e8c62aca820
Lok Cheuk <chunloklok912@gmail.com>
2022-12-16 17:19:09 +0800

    Update favLocList.js

--- src/screens/Home/normal/favLocList.js
+++ src/screens/Home/normal/favLocList.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import NormalUserLayout from "./normalUserLayout";
 
+const FavLocList = () => {
+    const [favLoc, setFavLoc] = useState([])
+    // const [uID, setUID] = useState("")
+    // console.log(localStorage.getItem('userid'))
+    // setUID(localStorage.getItem('userid'))
+    const uID = localStorage.getItem('userid')
+    console.log(uID)
 
-
-const FavLocList= ()=> {
-    const[favLoc, setFavLoc]=useState([])
-
-    useEffect(()=>{
-        fetch("http://18.209.252.141:13000/user/favouriteVenues/:userId   ")
-        .then((res)=>res.json())
-        .then((data)=>{
-            setFavLoc(data)
-            console.log(favLoc)
-        })
-    })
+    useEffect(() => {
+        fetch(`http://18.209.252.141:13000/user/favouriteVenues/${uID}`)
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data[0])
+                // console.log(data[0].favouriteVenues)
+                setFavLoc(data[0].favouriteVenues)
+                // console.log(favLoc)
+            })
+    }, [])
 
     return (
         <div>
@@ -26,6 +31,9 @@ const FavLocList= ()=> {
                         <div className='page-title-box'>
                             <div className='row mb-2'>
                                 <div className='col-6 col-sm-6'>
+                                    <a href={"/dashboard"} className="text-body pb-2" style={{ color: 'black' }}>
+                                        &laquo; Back
+                                    </a>
                                     <div className='page-title'> Favourite Location List</div>
                                 </div>
                             </div>
@@ -46,15 +54,14 @@ const FavLocList= ()=> {
                                         </thead>
                                         <tbody>
                                             {favLoc.map((item, index) => (
-                                                    <tr key={index}>
-                                                        <td width="10%" className="text-body">{item.id}</td>
-                                                        <td width="45%">
-                                                            <p className="m-0 d-inline-block align-middle font-16">
-                                                                <a href={"./location/" + item.id}  className="text-body" >{item.favouriteVenues}</a>
-                                                            </p>
-                                                        </td>
-                                                    </tr>
-                                                ))
+                                                <tr key={index}>
+                                                    <td width="45%">
+                                                        <p className="m-0 d-inline-block align-middle font-16">
+                                                            <a href={"./location/" + item.id} className="text-body" >{item.venuee}</a>
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                            ))
                                             }
                                         </tbody>
                                     </table>
@@ -67,7 +74,7 @@ const FavLocList= ()=> {
             </div>
         </div>
     )
-// }
+    // }
 
 }
 
